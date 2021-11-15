8b1c2877307764595abe5199a5b4dd8e9b4bffe5
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-15 09:53:39 +0100

    Stage 2/6: Ingredient calculator

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,7 +1,20 @@
-print('''Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!''')
+# Stage 1/6
+# print('''Starting to make a coffee
+# Grinding coffee beans
+# Boiling water
+# Mixing boiled water with crushed coffee beans
+# Pouring coffee into the cup
+# Pouring some milk into the cup
+# Coffee is ready!''')
+
+# ingredients per cup in ml
+water = 200
+milk = 50
+coffee_beans = 15  # grams
+
+number_of_cups = int(input('Write how many cups of coffee you will need:\n'))
+
+print(f'For {number_of_cups} cups of coffee you will need:')
+print(str(number_of_cups * water) + ' ml of water')
+print(str(number_of_cups * milk) + ' ml of milk')
+print(str(number_of_cups * coffee_beans) + ' g of coffee beans')
