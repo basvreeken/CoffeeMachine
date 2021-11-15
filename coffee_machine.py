bd5e4147a19ea6cca5394836a9f727c5a24a9194
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-15 10:13:02 +0100

    Stage 3/6: Estimate the number of servings

--- coffee_machine.py
+++ coffee_machine.py
@@ -12,9 +12,30 @@ water = 200
 milk = 50
 coffee_beans = 15  # grams
 
-number_of_cups = int(input('Write how many cups of coffee you will need:\n'))
+# Stage 2/6
+# number_of_cups = int(input('Write how many cups of coffee you will need:\n'))
+#
+# print(f'For {number_of_cups} cups of coffee you will need:')
+# print(str(number_of_cups * water) + ' ml of water')
+# print(str(number_of_cups * milk) + ' ml of milk')
+# print(str(number_of_cups * coffee_beans) + ' g of coffee beans')
 
-print(f'For {number_of_cups} cups of coffee you will need:')
-print(str(number_of_cups * water) + ' ml of water')
-print(str(number_of_cups * milk) + ' ml of milk')
-print(str(number_of_cups * coffee_beans) + ' g of coffee beans')
+water_available = int(input('Write how many ml of water the coffee machine '
+                            'has:\n'))
+milk_available = int(input('Write how many ml of milk the coffee machine '
+                           'has:\n'))
+coffee_beans_available = int(input('Write how many grams of coffee beans the '
+                                   'coffee machine has:\n'))
+cups_needed = int(input('Write how many cups of coffee you will need:\n'))
+maximum_capacity = [int(water_available / water), int(milk_available / milk),
+                    int(coffee_beans_available / coffee_beans)]
+maximum_cups = min(maximum_capacity)
+
+if cups_needed == maximum_cups:
+    print('Yes, I can make that amount of coffee')
+elif cups_needed > maximum_cups:
+    print(f'No, I can make only {maximum_cups} cups of coffee')
+elif cups_needed < maximum_cups:
+    n = maximum_cups - cups_needed
+    print(f'Yes, I can make that amount of coffee (and even {n} more than '
+          f'that)')
