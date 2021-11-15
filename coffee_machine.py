eef3a6547a8d7268c325b8f66b137102280ccddf
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-15 22:00:59 +0100

    Stage 5/6: Keep track of the supplies

--- coffee_machine.py
+++ coffee_machine.py
@@ -19,11 +19,12 @@ def print_state():
 
 
 def buy(product):
-    for i in range(0, 3):
-        coffee_machine[i] -= product[i]
-    coffee_machine[3] += product[3]
-    coffee_machine[4] -= 1
-    print_state()
+    if enough_resources(product):
+        for i in range(0, 3):
+            coffee_machine[i] -= product[i]
+        coffee_machine[3] += product[3]
+        coffee_machine[4] -= 1
+        print('I have enough resources, making you a coffee!')
 
 
 def fill(water, milk, beans, cups):
@@ -31,28 +32,51 @@ def fill(water, milk, beans, cups):
     coffee_machine[1] += milk
     coffee_machine[2] += beans
     coffee_machine[4] += cups
-    print_state()
 
 
 def take():
     print(f'I gave you ${coffee_machine[3]}')
     coffee_machine[3] = 0
-    print_state()
 
 
-print_state()
-action = input('Write action (buy, fill, take):\n')
+def enough_resources(product):
+    if coffee_machine[0] < product[0]:
+        print('Sorry, not enough water!')
+        return False
+    if coffee_machine[1] < product[1]:
+        print('Sorry, not enough milk!')
+        return False
+    if coffee_machine[2] < product[2]:
+        print('Sorry, not enough coffee beans!')
+        return False
+    if coffee_machine[4] < 1:
+        print('Sorry, not enough disposable cups!')
+        return False
+    else:
+        return True
 
-if action == 'buy':
-    choice = int(input('What do you want to buy? 1 - espresso, 2 - latte, '
-                       '3 - cappuccino: \n'))
-    buy(products[choice - 1])
-elif action == 'fill':
-    water = int(input('Write how many ml of water you want to add:\n'))
-    milk = int(input('Write how many ml of milk you want to add:\n'))
-    beans = int(input('Write how many grams of coffee beans you want to '
-                      'add:\n'))
-    cups = int(input('Write how many ml of coffee cups you want to add:\n'))
-    fill(water, milk, beans, cups)
-elif action == 'take':
-    take()
+
+while True:
+
+    action = input('Write action (buy, fill, take, remaining, exit):\n')
+
+    if action == 'buy':
+        choice = input('What do you want to buy? 1 - espresso, 2 - latte, '
+                       '3 - cappuccino: \n')
+        if choice == 'back':
+            continue
+        else:
+            buy(products[int(choice) - 1])
+    elif action == 'fill':
+        water = int(input('Write how many ml of water you want to add:\n'))
+        milk = int(input('Write how many ml of milk you want to add:\n'))
+        beans = int(input('Write how many grams of coffee beans you want to '
+                          'add:\n'))
+        cups = int(input('Write how many of coffee cups you want to add:\n'))
+        fill(water, milk, beans, cups)
+    elif action == 'take':
+        take()
+    elif action == 'remaining':
+        print_state()
+    elif action == 'exit':
+        break
